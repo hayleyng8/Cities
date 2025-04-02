7654522f852ffa163554230be5efb0503e0e1cea
hayleyng8 <162944181+hayleyng8@users.noreply.github.com>
2025-04-03 12:14:12 +1300

    Hashmap Basic Code
    Creating the Keys and values then using 'get' to grab the value from a key

---
+++ Cities.java
@@ -0,0 +1,23 @@
+import java.util.HashMap; // Import the HashMap Class
+/**
+ * Practice Hashmap Cities with Miss Wang
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Cities
+{
+    public static void main(String[] args){
+        HashMap<String, String> capCities = new HashMap<String, String>();
+        
+        // Add keys and values (countries and capCities)
+        capCities.put("New Zealand", "Wellington");
+        capCities.put("Australia", "Canberra");
+        capCities.put("Germany", "Berlin");
+        capCities.put("Norway", "Oslo");
+        capCities.put("USA", "Washington DC");
+        
+        System.out.println(capCities);
+        System.out.println(capCities.get("New Zealand"));
+    }
+}
